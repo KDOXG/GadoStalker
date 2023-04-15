d1bb6ccfc9d07a98ed1c70bb747e1ade89679ffa
thomaziogiacobbe <tgiacobbe@outlook.com>
2023-04-14 21:49:31 -0300

    fixed query

--- gadostalker_back/GadoStalker/src/main/java/com/ufpel/edu/br/gadostalker/model/repository/AnuncioRepository.java
+++ gadostalker_back/GadoStalker/src/main/java/com/ufpel/edu/br/gadostalker/model/repository/AnuncioRepository.java
@@ -3,7 +3,12 @@ package com.ufpel.edu.br.gadostalker.model.repository;
 import com.ufpel.edu.br.gadostalker.model.Anuncio;
 import com.ufpel.edu.br.gadostalker.model.Proprietario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AnuncioRepository extends JpaRepository<Anuncio, Long> {
-    Long countByProduto_Fazenda_ProprietarioAndDataFinalIsNullAndExcluidoIsFalse(Proprietario proprietario);
+    @Query("select count(a) from Anuncio a " +
+            "where a.produto.fazenda.proprietario.cpf = ?1 " +
+            "and a.dataFinal is null " +
+            "and a.isExcluido = false")
+    Long countAnunciosByProprietario(Proprietario proprietario);
 }
