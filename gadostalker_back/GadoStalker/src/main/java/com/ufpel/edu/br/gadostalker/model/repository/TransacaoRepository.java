dc5569b3c8b9ce92ceb4718297a2f9c59f87e793
thomaziogiacobbe <tgiacobbe@outlook.com>
2023-04-14 21:06:52 -0300

    created methods used in UsuarioController

--- gadostalker_back/GadoStalker/src/main/java/com/ufpel/edu/br/gadostalker/model/repository/TransacaoRepository.java
+++ gadostalker_back/GadoStalker/src/main/java/com/ufpel/edu/br/gadostalker/model/repository/TransacaoRepository.java
@@ -1,7 +1,11 @@
 package com.ufpel.edu.br.gadostalker.model.repository;
 
+import com.ufpel.edu.br.gadostalker.model.Proprietario;
 import com.ufpel.edu.br.gadostalker.model.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+    Long countByAnuncio_Produto_Fazenda_ProprietarioAndDataTransacaoBetween(Proprietario proprietario, Date dataInicial, Date dataFinal);
 }
