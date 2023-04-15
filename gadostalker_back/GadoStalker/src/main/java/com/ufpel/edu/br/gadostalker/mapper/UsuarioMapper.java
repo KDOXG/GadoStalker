f02035031114dc1d47d2c5a4d0ce1f17d2b7d80e
thomaziogiacobbe <tgiacobbe@outlook.com>
2023-04-14 22:27:21 -0300

    defined mappers for the rest of the subclasses of Usuario

--- gadostalker_back/GadoStalker/src/main/java/com/ufpel/edu/br/gadostalker/mapper/UsuarioMapper.java
+++ gadostalker_back/GadoStalker/src/main/java/com/ufpel/edu/br/gadostalker/mapper/UsuarioMapper.java
@@ -1,6 +1,10 @@
 package com.ufpel.edu.br.gadostalker.mapper;
 
+import com.ufpel.edu.br.gadostalker.dto.FuncionarioDTO;
+import com.ufpel.edu.br.gadostalker.dto.ProprietarioDTO;
 import com.ufpel.edu.br.gadostalker.dto.UsuarioComumDTO;
+import com.ufpel.edu.br.gadostalker.model.Funcionario;
+import com.ufpel.edu.br.gadostalker.model.Proprietario;
 import com.ufpel.edu.br.gadostalker.model.UsuarioComum;
 import org.mapstruct.Mapper;
 
@@ -8,4 +12,10 @@ import org.mapstruct.Mapper;
 public interface UsuarioMapper {
     UsuarioComumDTO toDTO(UsuarioComum usuarioComum);
     UsuarioComum toEntity(UsuarioComumDTO usuarioComumDTO);
+
+    FuncionarioDTO toDTO(Funcionario funcionario);
+    Funcionario toEntity(FuncionarioDTO funcionarioDTO);
+
+    ProprietarioDTO toDTO(Proprietario proprietario);
+    Proprietario toEntity(ProprietarioDTO proprietarioDTO);
 }
